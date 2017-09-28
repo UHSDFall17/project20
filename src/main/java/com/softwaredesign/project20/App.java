05a02a6b12187d344b47f441304e11112ff4b5ac
Saikrishna41 <saai.krrishna@gmail.com>
2017-09-28 12:54:08 -0500

    initial run for project 20

---
+++ src/main/java/com/softwaredesign/project20/App.java
@@ -0,0 +1,40 @@
+package com.softwaredesign.project20;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public void showOptions() {
+        System.out.println("1.Create Project");
+        System.out.println("2.Create Team");
+    }
+
+    public void optionChoose(int option) {
+
+        if(option == 1){
+
+        }
+        else if(option==2){
+
+        }
+        else if(option==3){
+
+        }
+    }
+
+    public void CreateMenu() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Create Menu.Please Choose your option");
+
+        Scanner in = new Scanner(System.in);
+
+        int option = in.nextInt();
+
+
+    }
+}
