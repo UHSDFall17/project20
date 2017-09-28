9fc98cf733e882d107e7a46d79cca8e7beb0f4b5
Saikrishna41 <saai.krrishna@gmail.com>
2017-09-28 13:57:35 -0500

    Create menu initial code from scratch

---
+++ src/main/java/CreateMenu.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class CreateMenu {
+
+    private static void displayOptions() {
+        System.out.println("Welcome!!Please Choose any one of the following options");
+
+        System.out.println("1.Create Board");
+        System.out.println("2.Create a new Team");
+
+    }
+
+    private static void chooseOptions(int option) {
+
+        if (option == 1) {
+            System.out.println("goto the Create board page");
+        } else if (option == 2) {
+            System.out.println("go to the Create a new team page");
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        displayOptions();
+        Scanner in = new Scanner(System.in);
+
+        int option = in.nextInt();
+        chooseOptions(option);
+
+
+    }
+}
