b67bb5c3eb9a646df54cd7e1be3d873501860629
Saikrishna41 <saai.krrishna@gmail.com>
2017-09-28 13:14:34 -0500

    changes n optionchoose functions. calling static methods. calling showoptions and optionChoose methods.

--- src/main/java/com/softwaredesign/project20/App.java
+++ src/main/java/com/softwaredesign/project20/App.java
@@ -6,34 +6,41 @@ import java.util.Scanner;
  * Hello world!
  */
 public class App {
-    public void showOptions() {
-        System.out.println("1.Create Project");
+    public static void showOptions() {
+        System.out.println("1.Create Board");
         System.out.println("2.Create Team");
-    }
-
-    public void optionChoose(int option) {
-
-        if(option == 1){
-
-        }
-        else if(option==2){
-
-        }
-        else if(option==3){
-
-        }
-    }
-
-    public void CreateMenu() {
 
     }
+
+
+    public static void optionChoose(int option) {
+
+        if (option == 1) {
+
+            System.out.println("go to create board page");
+        } else if (option == 2) {
+            System.out.println("go to create a new team page");
+        } else if (option == 3) {
+
+
+        }
+    }
+
+
+
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to Create Menu.Please Choose your option");
 
         Scanner in = new Scanner(System.in);
 
+
+        showOptions();
+
         int option = in.nextInt();
+
+        optionChoose(option);
 
 
     }
