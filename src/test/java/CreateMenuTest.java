d9c5771060d40ebe05fa2d81c8b9db8c0abbba3d
Saikrishna41 <saai.krrishna@gmail.com>
2017-10-15 23:01:26 -0500

    test cases initial

--- src/test/java/CreateMenuTest.java
+++ src/test/java/CreateMenuTest.java
@@ -1,8 +1,21 @@
+import org.junit.Test;
+
 import java.io.File;
 
 import static org.junit.Assert.*;
 
 public class CreateMenuTest {
+
+    private CreateMenu cm = new CreateMenu();
+
+
+    @Test
+    public void displayTeamTest() throws Exception {
+
+        assertTrue(cm.displayTeam("Team20"));
+    }
+
+
     @org.junit.Test
     public void main() throws Exception {
         File file = new File("teamprofile.txt");
