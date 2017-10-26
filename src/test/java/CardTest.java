e326dc5d2c6b0bbc232fec11717f57bd2a2c68c4
Saikrishna41 <saai.krrishna@gmail.com>
2017-10-26 14:53:28 -0500

    cardTest Intitial code

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class CardTest {
+    @Test
+    public void createCard() throws Exception {
+
+        File file = new File("cardCreate.txt");
+
+        assertTrue(file.canWrite());
+
+        assertTrue(file.canRead());
+
+
+
+    }
+
+    @Test
+    public void main() throws Exception {
+
+        File file = new File("cardCreate.txt");
+        assertTrue(file.exists());
+    }
+
+}
