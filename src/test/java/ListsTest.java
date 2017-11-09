85686d0a28629b6eb20b597c3b27cb9acc896ced
Saikrishna41 <saai.krrishna@gmail.com>
2017-11-09 15:51:05 -0600

    Test case for Lists

---
+++ src/test/java/ListsTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ListsTest {
+    @Test
+    public void createLists1() throws Exception {
+    }
+
+    @Test
+    public void createCard1() throws Exception {
+    }
+
+    @Test
+    public void main1() throws Exception {
+    }
+
+    @Test
+    public void createLists() throws Exception {
+    }
+
+    @Test
+    public void createCard() throws Exception {
+    }
+
+    @Test
+    public void main() throws Exception {
+    }
+
+}
