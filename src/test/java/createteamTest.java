e187352ea6a6d66384985cdd26421c6d39b9c6fe
Harshit Singh <32251746+Harshit1503@users.noreply.github.com>
2017-11-30 02:48:22 -0600

    Add files via upload

---
+++ src/test/java/createteamTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+public class createteamTest {
+    createteam ct = new createteam();
+
+   /* @Test
+    public void editTeam() throws Exception {
+    }*/
+
+    @Test
+    public void displayTeam() throws Exception {
+        assertTrue(ct.displayTeam());
+    }
+
+    @Test
+    public void displayTeamProfile() throws Exception {
+        assertTrue(ct.displayTeamProfile("second"));
+    }
+
+    @Test
+    public void displayTeamProfile1() throws Exception {
+        assertFalse(ct.displayTeamProfile("no such team"));
+    }
+
+  /*  @Test
+    public void addMembers() throws Exception {
+    }
+
+    @Test
+    public void createATeam() throws Exception {
+    }*/
+
+}
